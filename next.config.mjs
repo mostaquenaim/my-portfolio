6e60a33d3a0f72ecfacb47979011c3428a8e49e9
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2025-05-29 15:37:04 +0600

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        'flagcdn.com', // Add your image source domains here
+        // You can add any other domains from which you load images
+      ],
+      formats: ['image/webp', 'image/avif'], // Enable WebP and AVIF formats for optimization
+    },
+  };
+  
+  export default nextConfig;
+  
